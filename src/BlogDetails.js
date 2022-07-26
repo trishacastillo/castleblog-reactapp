2c0c51a62e8a65cfa077b90466f340c8c89d4c36
trishacastillo <79441676+trishacastillo@users.noreply.github.com>
2022-07-26 10:37:22 +0800

    patching the error

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -7,7 +7,7 @@ const BlogDetails = () => {
   const history = useHistory();
 
   const handleClick = () => {
-    fetch('http://localhost:8000/blogs/' + blog.id, {
+    fetch('http://localhost:3000/blogs/' + blog.id, {
       method: 'DELETE'
     }).then(() => {
       history.push('/');
